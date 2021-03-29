acda9a777ec2d52191ead3b77331bcff6f3a0d87
nickzheng87 <62922885+nickzheng87@users.noreply.github.com>
2021-03-30 04:32:53 +1100

    create map, player,, and movement and shooting.
    create map, player,, and movement and shooting.

---
+++ Assets/Script/Movement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float speed;
+    
+
+    public Rigidbody2D rb;
+    public Camera cam;
+    Vector2 position;
+    Vector2 mousePos;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+         position = transform.position;
+        position.x += moveX * speed * Time.deltaTime;
+        position.y += moveY * speed * Time.deltaTime;
+        transform.position = position;
+
+         mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+
+
+
+    }
+
+
+    private void FixedUpdate()
+    {
+        Vector2 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y,lookDir.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
+    }
+
+}
