9859dfc52f693c4c30b3053d280ea77ab6fb3abb
nickzheng87 <62922885+nickzheng87@users.noreply.github.com>
2021-03-31 17:56:59 +1100

    Add FSM
    add FSM to monster

---
+++ Assets/Script/bullet.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+
+
+    public GameObject Hiteffet;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameObject effect = Instantiate(Hiteffet, transform.position, Quaternion.identity);
+        Destroy(effect,1f);
+        Destroy(gameObject);
+    }
+
+}
